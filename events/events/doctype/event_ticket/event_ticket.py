bcdc02e45f19e8af8fb432ed1f6ca5bb96be6661
sai-dubey <tanoticoder@gmail.com>
2026-05-29 03:37:16 +0530

    feat: Enhance Event Ticket with QR code generation and additional fields
    Complated QR code Implemention to follow video 3 at timestamp 54:29 min

--- events/events/doctype/event_ticket/event_ticket.py
+++ events/events/doctype/event_ticket/event_ticket.py
@@ -1,23 +1,50 @@
 # Copyright (c) 2026, Tanoti and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class EventTicket(Document):
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
 
-	from typing import TYPE_CHECKING
+    from typing import TYPE_CHECKING
 
-	if TYPE_CHECKING:
-		from frappe.types import DF
+    if TYPE_CHECKING:
+        from frappe.types import DF
 
-		amended_from: DF.Link | None
-		attendee_name: DF.Data
-		booking: DF.Link | None
-		event: DF.Link
-		ticket_type: DF.Link
-	# end: auto-generated types
-	pass
+        amended_from: DF.Link | None
+        attendee_name: DF.Data
+        booking: DF.Link | None
+        event: DF.Link
+        qr_code: DF.AttachImage | None
+        ticket_type: DF.Link
+    # end: auto-generated types
+
+    def before_submit(self):
+        self.generate_qr_code()
+
+    def generate_qr_code(self):
+        import io
+
+        try:
+            import qrcode
+        except ImportError:
+            frappe.throw("qrcode library is not installed")
+
+        img = qrcode.make(f"{self.name}")
+        output = io.BytesIO()
+        img.save(output, format="PNG")
+        hex_data = output.getvalue()
+
+        qr_code_file = frappe.get_doc({
+            "doctype": "File",
+            "content": hex_data,
+            "attached_to_doctype": "Event Ticket",
+            "attached_to_name": self.name,
+            "attached_to_field": "qr_code",
+            "file_name": f"ticket-qr-code-{self.name}.png"
+        }).save()
+
+        self.qr_code = qr_code_file.file_url
